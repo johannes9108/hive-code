896d66a50b099e0d0162143f1390a192b90c6818
Johannes Hedman <johannes91.hedman@gmail.com>
2024-10-28 17:08:07 +0100

    flask endpoints returns correct data

--- container/flaskApp.py
+++ container/flaskApp.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
 from math import e
+import stat
+from turtle import st
 from flask import Flask
 from requests import get
 import aiohttp
@@ -11,7 +13,9 @@ app = Flask(__name__)
 async def fetch(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
-            return await response.json()
+            status = response.status
+            data = await response.json()
+            return status, data
 # API Client
 
 
@@ -25,36 +29,40 @@ def version():
 @app.route("/temperature")
 async def temperature():
     currentTime = datetime.now()
-    oneHourBefore = currentTime - timedelta(hours=1)
+    oneHourBefore = currentTime - timedelta(hours=2)
     currentTimeStr = currentTime.isoformat(timespec='seconds') + 'Z'
     oneHourBeforeStr = oneHourBefore.isoformat(timespec='seconds') + 'Z'
     print(oneHourBeforeStr)
     print(currentTimeStr)
     # 2024-10-28T13:45:00Z
-    # REQUEST =f"https://api.opensensemap.org/boxes?date={oneHourBeforeStr},{currentTimeStr}&phenomenon=temperature"
-    # data = await fetch(REQUEST)
+    REQUEST =f"https://api.opensensemap.org/boxes?date={oneHourBeforeStr},{currentTimeStr}&phenomenon=temperature"
+    status, data = await fetch(REQUEST)
     # Parse the JSON data
-    try:
-        with open('data-sample.json', 'r') as file:
-            data = file.read().strip()
-    except Exception:
-        print("Error reading version file")
-    jsonData = json.loads(data)
-    # print(jsonData[0]['sensors'])
-    # List to collect the values
-    temperature_values = []
+    # try:
+    #     with open('data-sample.json', 'r') as file:
+    #         data = file.read().strip()
+    # except Exception:
+    #     print("Error reading version file")
+    print(status, data[0])
+    if not data:
+        return "<p>No data found</p>"
+    else:
+        jsonData = data[0]
+        print(jsonData.get('sensors', []))
+        # List to collect the values
+        temperature_values = []
 
-    # Traverse the JSON structure
-    for item in jsonData[0].get("sensors", []):
-        if item.get("title", "").lower() in ["temperature", "temperatur"]:
-            print("Found temperature sensor")
-            if item.get("lastMeasurement", {}).get("value") is not None:
-                temperature_values.append(float(item.get("lastMeasurement", {}).get("value")))
+        # Traverse the JSON structure
+        for item in jsonData.get("sensors", []):
+            if item.get("title", "").lower() in ["temperature", "temperatur"]:
+                print("Found temperature sensor")
+                if item.get("lastMeasurement", {}).get("value") is not None:
+                    temperature_values.append(float(item.get("lastMeasurement", {}).get("value")))
 
-    # Print the collected values
-    print(temperature_values)
-    average = sum(temperature_values) / len(temperature_values)
+        # Print the collected values
+        print(temperature_values)
+        average = sum(temperature_values) / len(temperature_values)
+        
+        return f"<p>Values measured: {temperature_values}</p><p>Average: {average}</p>"
+
     
-    return f"<p>Values measured: {temperature_values}</p><p>Average: {average}</p>"
-
-
